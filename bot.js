edb1ea95a2e721d9e6448474d2d93d4515662653
Chris Dickinson <christopher.s.dickinson@gmail.com>
2017-02-13 16:49:46 -0800

    ops: add logging, metrics, configuration

--- bot.js
+++ bot.js
@@ -1,8 +1,36 @@
-const  Twit = require('twit');
-const T = new Twit(require('./secret'));
-const stream = T.stream('user');
+'use strict'
+
+var createMonitor = require('micro-monitor');
+var procMetrics = require('numbat-process');
+var Emitter = require('numbat-emitter');
+var bole = require('bole');
+var Twit = require('twit');
+
+var settings = require('./settings');
+
+createMonitor(settings.monitor_port);
+var logger = bole('bot');
+bole.output({
+  level: 'info',
+  stream: process.stdout
+});
+
+var T = new Twit({
+  access_token_secret: settings.access_token_secret,
+  access_token: settings.access_token,
+  consumer_secret: settings.consumer_secret,
+  consumer_key: settings.consumer_key
+});
+var stream = T.stream('user');
 
 var myFirstPublishSearch = {q: "#myfirstpublish", count: 10, result_type: "recent"}; 
+
+var metrics = new Emitter({
+  app: settings.name,
+  uri: settings.metrics
+});
+Emitter.setGlobalEmitter(metrics);
+procMetrics(metrics, 1000 * 30);
 
 stream.on('tweet', function (message) {
   const screenName = message.user.screen_name;
@@ -10,7 +38,15 @@ stream.on('tweet', function (message) {
 
   if (message.in_reply_to_screen_name === 'myfirstpublish') {
     T.post('statuses/update', { status: '@' + screenName + ' ' + congrats }, function(err, data, response) {
-      console.log(err);
+      logger.info('sent congrats to @' + screenName);
+      if (err) {
+        logger.error(err);
+      }
+      process.emit('metric', {
+        name: 'post',
+        success: Boolean(err)
+      });
     })
   }
 })
+
